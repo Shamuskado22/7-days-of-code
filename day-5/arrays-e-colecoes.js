078319c8c78b0c5730c6ecdd28f63331064ae941
samuel almeida <samucaoliveira1995@gmail.com>
2024-11-14 14:07:09 -0300

    feat: day 5 corrigido e finalizado

--- day-5/arrays-e-colecoes.js
+++ day-5/arrays-e-colecoes.js
@@ -1,14 +1,38 @@
-let lista = [
-  // Frutas
-  [],
-  // Laticínios
-  [],
-  // Congelados
-  [],
-  // Doces
-  [],
-]
+let frutas = []
+let laticinios = []
+let congelados = []
+let doces = []
+let adicionar = "sim"
 
-lista.push(["Maçã"])
+while (adicionar === "sim") {
+  adicionar = prompt(
+    "Deseja adicionar um item a lista de compras?\nDigite 'sim' para confirmar ou digite 'não' para cancelar"
+  )
 
-console.log(lista)
+  if (adicionar === "não" || adicionar === "nao") {
+    break
+  }
+
+  let alimento = prompt("Qual alimento quer adicionar?")
+  let categoria = prompt(
+    "Escolha o número de acordo com a categoria disponível:\n1. Frutas\n2. Laticínios\n3. Congelados\n4. Doces"
+  )
+
+  if (categoria == 1) {
+    frutas.push(alimento)
+  } else if (categoria == 2) {
+    laticinios.push(alimento)
+  } else if (categoria == 3) {
+    congelados.push(alimento)
+  } else if (categoria == 4) {
+    doces.push(alimento)
+  } else {
+    alert("Categoria não pré-definida")
+  }
+}
+
+alert(`Lista de compras:\n
+Frutas: ${frutas}\n
+Laticínios: ${laticinios}\n
+Congelados: ${congelados}\n
+Doces: ${doces}`)
